d6e3417f5eef5eca2c972877533eec173959f259
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2022-03-25 02:06:43 +0800

    Remove demo code

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import { Type } from "./ast/Type";
-import { Env } from "./Environment";
-import { Evaluate } from "./Evaluator";
-
-console.log(Evaluate.type(Type.implication(Type.atom('A'), Type.atom('A')), Env.default()))
